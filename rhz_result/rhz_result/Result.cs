eb1a14dc02bf97a31be5a0f97c481163a312dd35
Sascha Hampl <Sascha.Hampl@outlook.com>
2023-02-15 08:51:58 +0100

    implemented IResult<IReadOnlyCollection<T>>; fixed possible IResult.Error bug; nullcheck on Result.Failure<T>()

--- rhz_result/rhz_result/Result.cs
+++ rhz_result/rhz_result/Result.cs
@@ -36,24 +36,23 @@ public readonly struct Result<TValue> : IResult<TValue> {
     }
 
     public bool IsSuccess => !errorState;
-
     public bool IsError => errorState;
 
-    public Exception? Error => error;
+    public Exception Error => IsError ? error! : throw new InvalidOperationException("Cannot get Error when Result is not in Error State");
 
     public TValue Value => TryGetValue_ThrowExceptionOnFail(IsSuccess, value, error);
 
     private static T TryGetValue_ThrowExceptionOnFail<T>(bool isSuccess, T? value, Exception? error) {
         if (isSuccess) return value!;
         if (error is not null) throw error;
-        throw new InvalidOperationException($"Cannot get value when Result is in Error State.");
+        throw new InvalidOperationException($"Cannot get Value when Result is in Error State.");
     }
 }
-public interface IEnumerableResult<T> : IEnumerable<T>, IResult<IEnumerable<T>> { }
-public readonly struct EnumerableResult<TValue> : IEnumerableResult<TValue> {
-    private readonly Result<IEnumerable<TValue>> result;
+public interface IEnumerableResult<T> : IReadOnlyCollection<T>, IResult<IReadOnlyCollection<T>> { }
+public readonly struct EnumerableResult<TValue> : IReadOnlyCollection<TValue> {
+    private readonly Result<IReadOnlyCollection<TValue>> result;
 
-    public EnumerableResult(Result<IEnumerable<TValue>> result) {
+    public EnumerableResult(Result<IReadOnlyCollection<TValue>> result) {
         this.result = result;
     }
 
@@ -65,34 +64,40 @@ public readonly struct EnumerableResult<TValue> : IEnumerableResult<TValue> {
 
     public IEnumerable<TValue> Value => result.Value;
 
-    public IEnumerator<TValue> GetEnumerator() {
-        return GetEnumerator();
-    }
+    public int Count => result.Value.Count;
 
-    IEnumerator IEnumerable.GetEnumerator() {
-        return result.Value.GetEnumerator();
-    }
+    public IEnumerator<TValue> GetEnumerator() => GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => result.Value.GetEnumerator();
 }
 
 public static class Result {
-    public static IResult<T> Success<T>(T value) => 
+    public static IResult<T> Success<T>(T value) =>
         value is not null ? new Result<T>(value) : new Result<T>(new ArgumentNullException("Value cannot be null on creation of Result"));
-    public static IResult<T> Failure<T>(Exception exception) => new Result<T>(exception);
+    public static IResult<T> Failure<T>(Exception exception) =>
+        exception is not null ? new Result<T>(exception) : new Result<T>(new Exception());
+    public static IResult<T> Failure<T>() => new Result<T>(new Exception());
     public static IResult<T> Failure<T>(string message) => new Result<T>(message);
+}
+
+public static class EnumerableResult {
+    public static IEnumerableResult<T> Success<T>(IEnumerable<T> value) => (IEnumerableResult<T>)Result.Success((IReadOnlyCollection<T>)value.ToList().AsReadOnly());
+    public static IEnumerableResult<T> Success<T>(IReadOnlyCollection<T> value) => (IEnumerableResult<T>)Result.Success(value);
 }
 
 public static class ToIResultExtensions {
     public static IResult<T> ToResult<T>(this T value) => Result.Success(value);
     public static async Task<IResult<T>> ToResultAsync<T>(this Task<T> value) => Result.Success(await value);
-    public static IEnumerableResult<T> ToResult<T>(this IEnumerable<T> value) => Result.Success(value).AsEnumerable();
-    public static async Task<IEnumerableResult<T>> ToResultAsync<T>(this Task<IEnumerable<T>> value) => Result.Success(await value).AsEnumerable();
+    public static IEnumerableResult<T> ToResult<T>(this IEnumerable<T> value) => EnumerableResult.Success(value);
+    public static async Task<IEnumerableResult<T>> ToResultAsync<T>(this Task<IEnumerable<T>> value) => EnumerableResult.Success(await value);
 }
 
 public static class IResultExtensions {
     public static IResult<TOutput> Map<TInput, TOutput>(this IResult<TInput> input, Func<TInput, TOutput> func) =>
         input.IsError ? Result.Failure<TOutput>(input.Error) : Result.Success(func(input.Value));
 
-    public static IEnumerableResult<T> AsEnumerable<T>(this IResult<IEnumerable<T>> input) => (EnumerableResult<T>)input;
+    public static IEnumerableResult<T> AsEnumerable<T>(this IResult<IEnumerable<T>> input) => EnumerableResult.Success(input.Value);
+    public static IEnumerableResult<T> AsEnumerable<T>(this IResult<IReadOnlyCollection<T>> input) => EnumerableResult.Success(input.Value);
 
     public static IResult<T> Some<T>(this IResult<T> input, Func<T, T> func) =>
         input.IsSuccess ? Result.Success(func(input.Value)) : input;
@@ -101,7 +106,7 @@ public static class IResultExtensions {
         input.IsError ? Result.Success(func(input.Value)) : input;
 
     public static void Handle<T>(this IResult<T> input, Action<Exception> func) {
-        if(input.IsError) func(input.Error);
+        if (input.IsError) func(input.Error);
     }
 }
 
@@ -130,4 +135,7 @@ public static class IResultLoggingExtensions {
             logger.LogInformation(input.Error, input.Error.Message);
         return input;
     }
+}
+public static class IEnumerableResultExtensions {
+    public static IEnumerableResult<T> Where<T>(this IEnumerableResult<T> input, Func<T, bool> func) => EnumerableResult.Success(input.Value.Where(func));
 }
