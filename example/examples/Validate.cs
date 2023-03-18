807764889bbecb9dd91f51083a66f9a250884810
Sascha Hampl <Sascha.Hampl@outlook.com>
2023-03-18 13:07:24 +0100

    changed variable in validate from p to name to be more explicit.

--- example/examples/Validate.cs
+++ example/examples/Validate.cs
@@ -5,8 +5,8 @@ namespace example.examples;
 internal class Validate {
     internal static void Example(IResult<Person> okResult, IResult<Person> errorResult) {
         var v1 = Result.Ok("Harold")
-            .Validate(p => p == "Harold") //will execute and return a valid result with "Harold"
-            .Validate(p => p == "Steven"); //maps to an invalid result containing new ResultValidationException<T>(person)
+            .Validate(name => name == "Harold") //will execute and return a valid result with "Harold"
+            .Validate(name => name == "Steven"); //maps to an invalid result containing new ResultValidationException<T>(person)
         var v2 = errorResult.Validate(_ => false); //will not execute, because the result is already invalid
     }
 }
